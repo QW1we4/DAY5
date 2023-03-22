e110d9ae98bc0ee7e93b0ad3a8e985fb215b8e8f
QW1we4 <dkswogh468@gmail.com>
2023-03-22 11:55:57 +0900

    first commit

---
+++ variables.js
@@ -0,0 +1,9 @@
+let iAmProgrammer = true;
+let myFavoriteDrikIsZeroCoke = true;
+let iHaveAThousandBitcoins = false;
+
+if (iHaveAThousandBitcoins) {
+  console.log("You're a good person");
+} else {
+  console.log("You're a liar :(");
+}
